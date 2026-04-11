6d7ea75c782a92fe6cc6be257ea044966d52e2c1
Mestre Verde <6carlosgomes4@gmail.com>
2026-04-11 22:01:49 +0100

    minhas alterações locais

---
+++ bibliotecas/Data_Types/data_types.c
@@ -0,0 +1,11 @@
+#include "data_types"
+
+#include <stdio.h>
+        
+
+void printSizeof() {
+    printf("format\t\t\tnº of bytes\t2^n")
+  for (i = 0; i < maxValue; i++) {
+    printf("%c\t\t\t%zu\t%i",datatypes[i],sizeof(vartype[i]),)
+  }
+}
--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
 #include <dirent.h>   // biblioteca para mexer com pastas
 #include <stdbool.h>  // inclui o bool
 #include <stdio.h>
+#include "data_types.h"
+
+// Paths
+const signed char dataFolder[] = "/home/MestreVerde/Programação/Projetos_de_Programação/Invent-rio-componentes-el-tricos-main/data";
+
+enum componenteSection { NONE = 0, Resistencia = 1, Condensador = 2, Bobina = 3 };
 
 /**
  * @brief Verifica a integridade do inventário
@@ -9,12 +15,24 @@
  * @author M.V.
  */
 _Bool setup() {
+  DIR* dir = opendir(dataFolder);
+
+  if (dir == NULL) {
+    perror("Não foi possível abrir a pasta");
+    return 1;
+  }
+
+  struct dirent* entry;
+  while ((entry = readdir(dir)) != NULL) {
+    printf("%s\n", entry->d_name);  // imprime cada arquivo/pasta
+  }
+
+  closedir(dir);  // fecha o diretório
+  return 0;
 }
 
 int main(void) {
   if (setup()) goto end;
-
-
 
 end:
   return 0;
